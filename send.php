753be09a6b1684a424736964c49e8a905b342a80
Senicheff <office@amirum.ru>
2023-04-26 18:06:55 +0300

    callback on email + works

---
+++ send.php
@@ -0,0 +1,72 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+
+// $email = $_POST['email'];
+// $text = $_POST['text'];
+// $file = $_FILES['myfile'];
+
+// Формирование самого письма
+$title = "Заявка с сайта Анжи-Веб на обратный звонок";
+$body = "
+<h2>Новая заявка на звонок!</h2>
+<b>Имя:</b> $name<br>
+<b>Телефон:</b> $phone
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.yandex.ru'; // SMTP сервера вашей почты
+    $mail->Username   = 'office@amirum.ru'; // Логин на почте
+    $mail->Password   = 'zdmxwmsgexurwiem'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('office@amirum.ru', 'Анжи-Веб'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('office@amirum.ru');  
+    // $mail->addAddress('youremail@gmail.com'); // Ещё один, если нужен
+
+    // Прикрипление файлов к письму
+if (!empty($file['name'][0])) {
+    for ($ct = 0; $ct < count($file['tmp_name']); $ct++) {
+        $uploadfile = tempnam(sys_get_temp_dir(), sha1($file['name'][$ct]));
+        $filename = $file['name'][$ct];
+        if (move_uploaded_file($file['tmp_name'][$ct], $uploadfile)) {
+            $mail->addAttachment($uploadfile, $filename);
+            $rfile[] = "Файл $filename прикреплён";
+        } else {
+            $rfile[] = "Не удалось прикрепить файл $filename";
+        }
+    }   
+}
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
