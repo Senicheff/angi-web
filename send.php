b819231db352fd74ffae1723f3d06af6c4a84016
Senicheff <dmitry@senicheff.ru>
2023-04-27 08:52:54 +0300

    Update send.php
    delete email password

--- send.php
+++ send.php
@@ -32,7 +32,7 @@ try {
     // Настройки вашей почты
     $mail->Host       = 'smtp.yandex.ru'; // SMTP сервера вашей почты
     $mail->Username   = 'office@amirum.ru'; // Логин на почте
-    $mail->Password   = 'zdmxwmsgexurwiem'; // Пароль на почте
+    $mail->Password   = ''; // Пароль на почте
     $mail->SMTPSecure = 'ssl';
     $mail->Port       = 465;
     $mail->setFrom('office@amirum.ru', 'Анжи-Веб'); // Адрес самой почты и имя отправителя
